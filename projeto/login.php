ca8b941e2f9a4da921e396aec8d980934663a780
Eveton22 <eds.silva292@gmail.com>
2024-02-09 19:16:29 -0300

    first commit

--- projeto/login.php
+++ projeto/login.php
@@ -20,81 +20,62 @@
 
         header("Location: logado.php");
     }
-
 ?>
-
 <!DOCTYPE html>
-</html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>D.ENE.A</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Denea Cosméticos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href=".css">
+    <link rel="stylesheet" href="styles.scss">
 </head>
-<body class="col-12 "style="background-image: url('https://www.denea.com.br/stores/784/static/img/back_image.png'); display:flex; justify-content:center;">
-
-    <div class="benner vh-100 "  >
-        <div style=" padding-top: 60px; display: flex; justify-content:center; aling-items:center;">
-            <div  style="display-flex; justify-content: center; ">
-                <img  class="logo pb-3"src="https://www.denea.com.br/stores/784/static/img/logo-black-large.png" alt="">
+<body>
+    <div class="container-fluid">
+        <div class="container-todos">
+            <div class="d-flex justify-content-center imglogo">
+                <img src="assets/logoPrincipal.png" alt="">
             </div>
-        </div>
-        <div class="banner lign-items-center shadow-lg p-3 mb-4 bg-white rounded mt-2" style="width: 1300px; background-image: url('https://www.denea.com.br/stores/784/static/img/wave-pink.png'); height: 410px" >
-            <div class="row container banner-content">
-                <div class="col-lg-5 col-sm-12 pt-5 m-0" >
-                    <div class="title-banner">
-                        <h1> BEM VINDO À D.ENE.A</h1>
-                        <h3> Cosmetidos Profissionais</h3>
-                    </div>
-                    <div class="text-banner w-100">
-                        <p>Em breve, você terá acesso a uma gama exclusiva de produtos que cuidam da integridade, saúde
-                                e regeneração dos seus cabelos.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-12 pt-0 mt-5">
-                    <div class="img-banner ">
-                        <img src="https://www.denea.com.br/stores/784/static/img/mockyup%202.png" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-12 pt-3 m-0 w-5" style="width: 370px;">
-                    <div class="form-banner">
-                        <div>
-                            <p>Preencha o formulário abaixo com o seu nome e e-mail para receber novidades e aproveitar um
-                                <strong> desconto especial na sua primeira compra!</strong>
-                            </p>
+            <div class="div01">
+                <div class="elementos">
+                    <div class="box-elementos shadow-lg p-3 mb-5">
+                        <div class="todoselementos">
+                            <div class="div1">
+                                <h1>BEM VINDO À D.ENEA</h1>
+                                <h4>Cosmesticos Profissionais</h3>
+            
+                                <span>Em breve, você terá acesso a uma gama exclusiva de<br /> produtos que cuidam da integridade, saúde e regeneração<br /> dos seus cabelos.</span>
+                            </div>
+                            <div class="div2">
+                                <img src="assets/produtos.png" alt="">
+                            </div>
+                            <div class="div-form">
+                                <p>Preencha o formulário abaixo com o seu nome<br /> e e-mail para receber novidades e aproveitar<br />
+                                um <span>desconto especial na sua primeira compra!</span></p>
+                                <form action="" method="POST">
+                                    <div>
+                                        <label for="">SEU NOME</label> 
+                                        <input class="input form-control" type="text" name="nome" placeholder="Nome">
+                                    </div>
+                                    <div class="input2">
+                                        <label for="">SEU E-MAIL</label> 
+                                        <input class="input form-control" type="email" name="email" placeholder="E-mail">
+                                    </div>
+                                    <div class="btn">
+                                        <input name="enviar" type="submit" value="QUERO ME INSCREVER" class="btn btn-dna text-white mt-3 fw-bold" />
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <form action="" method="POST">
-                            <div class="mb-3 mt-3 form-row">
-                                <label for="" class="form-label fw-bold">
-                                    SEU NOME
-                                </label>
-                                <input type="text" required class="form-control" name="nome" placeholder="Nome">
-                            </div>
-                            <div class="mb-3 form-row fw-bold">
-                                <label for="" class="form-label">
-                                    SEU E-MAIL
-                                </label>
-                                <input type="email" required  class="form-control" name="email" placeholder="E-mail">
-                            </div>
-                            <input name="enviar" type="submit" value="QUERO ME INSCREVER" class="btn btn-dna bg-dark text-white mt-3" />
-                        </form>
                     </div>
                 </div>
+                </div>
+                <div class="footer">
+                <p><span>Profissional</span> como você sempre quis, <span>acessível</span> como você nunca viu.</p>
+                <p class="p"> O melhor está por vir, aguarde!</p>
             </div>
         </div>
-        <div class="row p-0 m-0 w-100">
-            <div class="col-12 text-center">
-                    <p><strong>Profissional</strong> como você sempre quis,<strong>acessível</strong> como você nunca viu.
-                    <p class="" style="position: relative; top:-20px">O melhor está por vir, aguarde!</p>
-                </div>
-            </div>
     </div>
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
